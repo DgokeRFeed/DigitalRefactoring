37cd1f8ff3702f2c306dd3078b341dbfd16b59bb
DgokeRFeed <popovvova99@yandex.ru>
2025-01-22 02:21:26 +0300

    refactoring

--- digitalhr.rb
+++ digitalhr.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Исправил связи на 'has_and_belongs_to_many',
+  # поскольку связи между сущностями 'many-to-many',
+  # а взаимодействовать с промежуточной моделью нам не надо
+
   has_and_belongs_to_many :interests
   has_and_belongs_to_many :skills
 end
@@ -11,51 +15,59 @@ class Skill < ApplicationRecord
   has_and_belongs_to_many :users
 end
 
-module Users
+odule Users
   class Create < ActiveInteraction::Base
-    hash :params do
-      string :name, :patronymic, :email,
-        :nationality, :country, :gender, :skills
+    string :name, :surname, :patronymic, :email,
+      :nationality, :country, :gender, :skills
+    integer :age
+    array :interests
 
-      integer :age
+    # вместо guard clause лучше использовать валидаторы
+    validates(
+      :name,
+      :surname,
+      :patronymic,
+      :email,
+      :age,
+      :gender,
+      :country,
+      :nationality,
+      :interests,
+      :skills,
+      presence: true
+    )
 
-      array :interests
-
-      validates(
-        :name,
-        :patronymic,
-        :email,
-        :nationality,
-        :country,
-        :gender,
-        :age,
-        presence: true
-      )
-
-      validates :age, compartion: { greater_than: 0, less_than_or_equal_to: 90 }
-      validates :gender, inclusion: { in: %w[male female] }
-    end
+    validates :email, uniqueness: true
+    validates :gender, inclusion: { in: %w[male female] }
+    validates :age, comparison: { greater_than: 0, less_than_or_equal_to: 90 }
 
     def execute
-      return if User.where(email: params['email'])
+      # вместо интерполяции использовал join(' ')
+      full_name = [surname, name, patronymic].join(' ')
 
-      full_name =
-        [
-          params['surname'],
-          params['name'],
-          params['patronymic']
-        ]
-        .map!(&:capitalize)
-        .join(' ')
+      user_params = {
+        name:,
+        surname:,
+        patronymic:,
+        full_name:,
+        email:,
+        age:,
+        gender:,
+        country:,
+        nationality:
+      }
 
-      user_params = params.except(:interests, :skills).merge(full_name:)
+      # вместо create использовал new, чтобы не создавать объект в базе сразу
       user = User.new(user_params)
 
-      user.interests = Interest.where(name: params['interests'])
+      # добавляя по одному объекту interest будет N+1
+      user.interests = Interest.where(name: interests)
 
-      user_skills = params['skills'].split(',')
+      # Skil.find(name: skil) внутри массива даст N+1
+      user_skills = skills.split(',')
       user.skills = Skill.where(name: user_skills)
 
+      # если объект не валиден, отдаем ошибки
       unless user.save
         errors.merge!(user.errors)
       end
@@ -65,61 +77,24 @@ module Users
   end
 end
 
+# 2. Иправление опечатки Skil
+#
+# Вариант 1: использовать rename_table
+#
+# def change
+#   rename_table :skils, :skills
+# end
+#
+# Вариант 2: создать новую таблицу, перенести данные, удалить старую таблицу
+#
+# def up
+#   create_table :skills do |t|
+#     t.name :string, null: false
+#   end
+#
+#   execute 'INSERT INTO skills SELECT * FROM :skils'
+# end
+#
+# удалить старую таблицу лучше потом, удостоверившись, что данные перенесены
+
 ##################################################
-
-class User < ApplicationRecord
-  has_many :interests
-  has_many :skills, class_name: 'Skil'
-end
-
-class Interest < ApplicationRecord
-  has_many :users
-end
-
-class Skil < ApplicationRecord
-  has_many :users
-end
-
-class Users::Create < ActiveInteraction::Base
-  hash :params
-
-  def execute
-    #don't do anything if params is empty
-    return unless params['name']
-    return unless params['patronymic']
-    return unless params['email']
-    return unless params['age']
-    return unless params['nationality']
-    return unless params['country']
-    return unless params['gender']
-    ##########
-    return if User.where(email: params['email'])
-    return if params['age'] <= 0 || params['age'] > 90
-    return if params['gender'] != 'male' or params['gender'] != female
-
-    user_full_name = "#{params['surname']} #{params['name']} #{params['patronymic']}"
-    user_params = params.except(:interests)
-    user = User.create(user_params.merge(user_full_name))
-
-    Intereset.where(name: params['interests']).each do |interest|
-      user.interests = user.interest + interest
-      user.save!
-    end
-
-    user_skills = []
-    params['skills'].split(',').each do |skil|
-      skil = Skil.find(name: skil)
-      user_skills =  user_skills + [skil]
-    end
-    user.skills = user_skills
-    user.save
-  end
-end
-
-# Нужно:
-# 1. Использовать gem ActiveInteraction => https://github.com/AaronLasseigne/active_interaction отрефакторить класс Users::Create
-# 2. Исправить опечатку Skil. Есть 2 пути решения. Описать оба.
-# 3. Исправить связи
-# 4. Поднять Rails приложение и в нем использовать класс Users::Create
-# 5. Написать тесты
-# 6. При рефакторнге кода использовать Декларативное описание(подход в программировании)
